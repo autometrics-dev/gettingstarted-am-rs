83bdef4499e59681cd008d82f37229fa86a7fc28
Mari <me@cutegirl.tech>
2023-12-06 11:10:34 +0100

    update all dependencies (#3)
    * update to autometrics-rs 1.0.0
    
    * update rest of the dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use axum::http::StatusCode;
-use axum::response::IntoResponse;
-use axum::routing::get;
-use axum::Router;
-use std::env;
-use std::time::Duration;
-use tokio::time::sleep;
-
 use autometrics::{
     autometrics,
     objectives::{Objective, ObjectiveLatency, ObjectivePercentile},
     prometheus_exporter,
 };
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::get;
+use axum::Router;
+use std::env;
+use std::error::Error;
+use std::time::Duration;
+use tokio::net::TcpListener;
+use tokio::time::sleep;
 
 /// This is a Service-Level Objective (SLO) we're defining for our API.
 /// It's a combination of a success rate and latency.
@@ -21,7 +22,7 @@ const API_SLO: Objective = Objective::new("api")
     .latency(ObjectiveLatency::Ms250, ObjectivePercentile::P90);
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     // Set up the exporter to collect metrics
     prometheus_exporter::init();
 
@@ -29,51 +30,49 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         .route("/", get(index))
         .route("/slow", get(slow_function))
         .route("/error", get(error_function))
-        .route(
-            "/metrics",
-            get(|| async { prometheus_exporter::encode_http_response() }),
-        );
+        .route("/metrics", get(metrics));
 
-    let addr = env::var("LISTEN_ADDRESS")
-        .unwrap_or_else(|_| "127.0.0.1:3000".to_string())
-        .parse()?;
+    let addr = env::var("LISTEN_ADDRESS").unwrap_or_else(|_| "127.0.0.1:3000".to_string());
 
-    let server = axum::Server::try_bind(&addr)?.serve(app.into_make_service());
-
-    let local_addr = server.local_addr();
+    let listener = TcpListener::bind(&addr).await?;
+    let local_addr = listener.local_addr()?;
 
     eprintln!("Listening on: {local_addr}",);
-    eprintln!("");
+    eprintln!();
     eprintln!("The following endpoints are available: ");
-    eprintln!("");
+    eprintln!();
     eprintln!("- http://{local_addr}/        | static 200 response",);
     eprintln!("- http://{local_addr}/slow    | same but it is delayed with 1 second",);
     eprintln!("- http://{local_addr}/error   | static 500 response",);
     eprintln!("- http://{local_addr}/metrics | Prometheus endpoint containing the metrics",);
-    eprintln!("");
+    eprintln!();
     eprintln!("To see the metrics in Explorer run: `am start {local_addr}`");
 
     // Start accepting and handling requests
-    server.await?;
+    axum::serve(listener, app).await?;
 
     Ok(())
 }
 
 // our main handler function that is fine
 #[autometrics(objective = API_SLO)]
-pub async fn index() -> impl IntoResponse {
-    return "Hello, World!";
+async fn index() -> impl IntoResponse {
+    "Hello, World!"
 }
 
 // our slow function that is slow
 #[autometrics(objective = API_SLO)]
-pub async fn slow_function() -> impl IntoResponse {
+async fn slow_function() -> impl IntoResponse {
     sleep(Duration::from_millis(1000)).await;
-    return "Hello, World again!";
+    "Hello, World again!"
 }
 
 // our error function that errors
 #[autometrics(objective = API_SLO)]
-pub async fn error_function() -> Result<String, StatusCode> {
-    return Err(StatusCode::INTERNAL_SERVER_ERROR);
+async fn error_function() -> Result<String, StatusCode> {
+    Err(StatusCode::INTERNAL_SERVER_ERROR)
+}
+
+async fn metrics() -> Result<String, StatusCode> {
+    prometheus_exporter::encode_to_string().map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
 }
